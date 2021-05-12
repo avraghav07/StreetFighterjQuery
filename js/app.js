b8de55e212351e7cbb483f8295e8b09e60ccd9dd
Raghav <zCalibeRz1337@gmail.com>
2021-05-12 17:11:46 +0530

    working

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    function playHadouken() {
+        $("#hadouken-sound")[0].volume = 0.5;
+        $("#hadouken-sound")[0].load();
+        $("#hadouken-sound")[0].play();
+    }
+
+    $(".ryu").mouseenter(function() {
+        $(".ryu-still").hide();
+        $(".ryu-ready").show();
+    });
+
+    $(".ryu").mouseleave(function() {
+        $(".ryu-ready").hide();
+        $(".ryu-still").show();
+    })
+
+    .mousedown(function() {
+        playHadouken();
+        console.log("mousedown");
+        $(".ryu-ready").hide();
+        $(".ryu-throwing").show();
+        $(".hadouken").finish().show()
+        .animate (
+            {"left":"1250px"},
+            500,
+            function() {
+                $(".hadouken").hide();
+                $(".hadouken").css("left", "520px");
+            }
+        )
+    })
+
+    .moouseup(function() {
+        console.log("mouseup");
+        $(".ryu-throwing").hide();
+        $(".ryu-ready").show();
+    })
+});
